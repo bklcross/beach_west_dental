85753223556922e48fbd6a43bb08ee1fa323eb04
Serofina Lee <serofina.lee@gmail.com>
2021-10-08 22:10:03 -0700

    Taco

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Nav from "./components/Nav";
 import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div className="App">
       <Nav />
       <Card />
+      <Footer />
+      
     </div>
   );
 }
