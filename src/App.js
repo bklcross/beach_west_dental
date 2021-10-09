0ce4c0f5887f3ccff72f8b9cafd2d22debfe5643
DimitriMatevossian <dimitri.matevossian@gmail.com>
2021-10-08 22:00:10 -0700

    made test card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Nav from "./components/Nav";
+import Card from "./components/Card";
 
 export default function App() {
   return (
     <div className="App">
       <Nav />
+      <Card />
     </div>
   );
 }
