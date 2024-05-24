a27e38d2af53603bbba02514015df768ec0bf5ac
bklcross <bklcross@gmail.com>
2024-05-24 09:29:30 -0700

    add cors

--- amplify/backend/function/recaptcha/src/app.js
+++ amplify/backend/function/recaptcha/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,6 +13,31 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "*");
   next();
 });
+
+const allowedOrigins = [
+  "https://beachwestdental.com",
+  "https://dev.d28l2mrgjucolx.amplifyapp.com",
+  "http://localhost:5173",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    optionsSuccessStatus: 200,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
+    preflightContinue: false,
+  })
+);
 
 app.post("/verify-recaptcha", async (req, res) => {
   const { token } = req.body;
