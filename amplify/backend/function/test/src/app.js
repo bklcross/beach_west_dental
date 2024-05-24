df8a798c3b0d231addf653c83a578bab974477f7
bklcross <bklcross@gmail.com>
2024-05-24 14:35:29 -0700

    update cors

--- amplify/backend/function/test/src/app.js
+++ amplify/backend/function/test/src/app.js
@@ -16,7 +16,7 @@ app.use(awsServerlessExpressMiddleware.eventContext());
 
 // Enable CORS for all methods
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
   res.header("Access-Control-Allow-Headers", "*");
   next();
 });
